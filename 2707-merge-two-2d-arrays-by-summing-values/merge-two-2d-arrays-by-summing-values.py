8b6178da0c006475056a69772b5db1abf0d2e99e
sharmamukesh04 <83703781+sharmamukesh04@users.noreply.github.com>
2025-06-17 22:37:22 +0530

    Time: 4 ms (10.27%) | Memory: 17.9 MB (87.86%) - LeetSync

--- 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.py
+++ 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.py
@@ -1,29 +1,40 @@
 class Solution:
     def mergeArrays(self, nums1: List[List[int]], nums2: List[List[int]]) -> List[List[int]]:
 
+        i = 0
+        j = 0
         ans = []
-        mpp = {}
-
-        for l in nums1:
-            if l[0] not in mpp:
-                mpp[l[0]] = []
-
-            mpp[l[0]].append(l[1])
-
-        for l in nums2:
-            if l[0] not in mpp:
-                mpp[l[0]] = []
-
-            mpp[l[0]].append(l[1])
-
-        for key, values in mpp.items():
+        while i<len(nums1) and j<len(nums2):
             sum = 0
-            for num in values:
-                sum+=num
-            
-            ans.append([key, sum])
+            if nums1[i][0]>nums2[j][0]:
+                ans.append([nums2[j][0],nums2[j][1]])
+                j+=1
+            elif nums1[i][0]<nums2[j][0]:
+                ans.append([nums1[i][0],nums1[i][1]])
+                i+=1
+            flag = False
+            key = -1
+            while i<len(nums1) and j<len(nums2) and nums1[i][0] == nums2[j][0]:
+                flag = True
+                if key !=-1 and key != nums1[i][0]:
+                    break
+                key = nums1[i][0]
+                sum+=nums1[i][1]+nums2[j][1]
+                i+=1
+                j+=1
+            if flag:
+                ans.append([key,sum])
+                flag = False
+                sum = 0
+        
+        while i<len(nums1):
+            ans.append([nums1[i][0],nums1[i][1]])
+            i+=1
+        
+        while j<len(nums2):
+            ans.append([nums2[j][0],nums2[j][1]])
+            j+=1
 
-        ans.sort()
         return ans
 
         
